c5615123307963c7fbb1c72d666580c5928a232b
sukritx <ddserial@gmail.com>
2025-04-19 17:33:37 +0700

    change file name

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/userRouter');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
