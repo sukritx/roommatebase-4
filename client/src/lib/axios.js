190c6e43d65c5f569f4faad96a3d8f941dcb4408
sukritx <ddserial@gmail.com>
2025-05-17 23:32:26 +0700

    change axios base url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", // Change to your API URL if needed
+  baseURL: "http://localhost:5000/api", // Change to your API URL if needed
   headers: {
     "Content-Type": "application/json",
   },
