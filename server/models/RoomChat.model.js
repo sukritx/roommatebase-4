fb265dc9d09f88c4f5aa8f771fce2686b2589c6c
sukritx <ddserial@gmail.com>
2025-07-03 22:47:49 +0700

    add roomChat schema

--- server/models/RoomChat.model.js
+++ server/models/RoomChat.model.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const RoomChatSchema = new mongoose.Schema({
   room: { type: Schema.Types.ObjectId, ref: 'Room', required: true },
-  senderId: {
-    type: Schema.Types.ObjectId, ref: 'User',
-    required: true
+  sender: {
+    senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    username: { type: String, required: true },
   },
   chatContent: {
     type: String,
@@ -14,7 +14,12 @@ const RoomChatSchema = new mongoose.Schema({
   replyParentMessage: {
     type: Schema.Types.ObjectId, ref: 'RoomChat',
     default: null
-  }
+  },
+  isDeleted: { type: Boolean, default: false, index: true }, // Soft delete
+  deletedBy: { type: Schema.Types.ObjectId, ref: 'User' }, // Moderator ID
+  deletionReason: { type: String },
 }, { timestamps: true });
+
+RoomChatSchema.index({ room: 1, createdAt: 1 });
 
 module.exports = mongoose.model('RoomChat', RoomChatSchema);
